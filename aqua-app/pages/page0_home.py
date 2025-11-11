98efb1fb7223207f95a8836257417a9cdb4671cc
Victoria Hollingshead <victholl@sls-315-m1-2.local>
2025-11-10 17:59:35 -0800

    add root

--- aqua-app/pages/page0_home.py
+++ aqua-app/pages/page0_home.py
@@ -9,17 +9,18 @@ from pyprojroot import here
 # set root directory
 root = here()
 
+print('root directory:')
 print(root)
 
-# spinner until the entire page is loaded
-with st.spinner("Loading page..."):
-    time.sleep(3)
+# # spinner until the entire page is loaded
+# with st.spinner("Loading page..."):
+#     time.sleep(3)
 
-# @st.cache_data(show_spinner="Loading map...")
-# insert html map from /Users/victholl/Documents/aquaculture_chile/vict_playground/concession_eda.html
-def load_html_map():
-    with open(root / 'media' / 'concession_eda_production_bubble_map2.html', 'r') as file:
-        html_map = file.read()
-    return html_map
-st.title("Welcome to the Chile Aquaculture Project")
-st.components.v1.html(load_html_map(), width="100%", height=900)
+# # @st.cache_data(show_spinner="Loading map...")
+# # insert html map from /Users/victholl/Documents/aquaculture_chile/vict_playground/concession_eda.html
+# def load_html_map():
+#     with open(root / 'media' / 'concession_eda_production_bubble_map2.html', 'r') as file:
+#         html_map = file.read()
+#     return html_map
+# st.title("Welcome to the Chile Aquaculture Project")
+# st.components.v1.html(load_html_map(), width="100%", height=900)
