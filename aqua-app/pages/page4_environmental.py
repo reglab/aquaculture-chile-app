4301fedc108453d716b4dfbbcd50e56f6697bb4a
Victoria Hollingshead <victholl@sls-315-m1-2.local>
2025-11-10 18:43:04 -0800

    removed folder

--- aqua-app/pages/page4_environmental.py
+++
@@ -1,6 +0,0 @@
-
-import streamlit as st
-
-
-st.write("Click below for our questions for Environmental Impacts data:")
-st.link_button("Environmental Impacts", "https://docs.google.com/document/d/1wk89FpoxZWU-6h1iE_SfOrqOxOFTkZpSqFq0C3usdMc/edit?tab=t.0")
