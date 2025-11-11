ea2678b3d6483afe465c152d6e648982ed079901
Victoria Hollingshead <103971332+vhollingshead@users.noreply.github.com>
2025-11-11 04:23:55 +0000

    test

--- pages/page2_sensors.py
+++ pages/page2_sensors.py
@@ -1,10 +1,11 @@
 import plotly.express as px
 import streamlit as st
 
+st.write("test")
 
 
-with open('media/disconnection_map.html', 'r') as file:
-    html_map_1 = file.read()
+# with open('media/disconnection_map.html', 'r') as file:
+#     html_map_1 = file.read()
 
-# make map bigger height and width
-st.components.v1.html(html_map_1, width="100%", height=1000)
+# # make map bigger height and width
+# st.components.v1.html(html_map_1, width="100%", height=1000)
