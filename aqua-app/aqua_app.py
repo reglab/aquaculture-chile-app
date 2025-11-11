919949b926b3098c1553a68c480854bc23ca0366
Victoria Hollingshead <victholl@sls-315-m1-2.local>
2025-11-10 17:57:42 -0800

    updated pages

--- aqua-app/aqua_app.py
+++ aqua-app/aqua_app.py
@@ -8,56 +8,62 @@ from folium import plugins
 import geopandas as gpd
 from PIL import Image
 
-# Set page config
+import pickle
+from pathlib import Path
+import streamlit_authenticator as stauth
+
 st.set_page_config(
     page_title="Chile Aquaculture Data",
     page_icon="🐟",
     layout="wide"
 )
 
-# make multipage streamlit app appear on sidebar not selectbox
-
 pages = {
-    "Data Exploration": [
-        st.Page('pages/page0_home.py', title="Home"),
-        st.Page('pages/page1_salmon_complaints.py', title="Salmon Concessions & Complaints"),
-        st.Page('pages/page2_sensors.py', title="Sensors"),
-        st.Page('pages/page3_production.py', title="Production"),
-        st.Page('pages/page3b_production_movement.py', title="Ownership and Production Trends"),
-        st.Page('pages/page4_environmental.py', title="Environmental Impacts")
-        
-    ],
-    "External Datasets": [
-        st.Page('pages/page5_other.py', title="Other Visualizations")
-    ],
-}
+        "Data Exploration": [
+            st.Page('pages/page0_home.py', title="Home"),
+            st.Page('pages/page1_salmon_complaints.py', title="Salmon Concessions & Complaints"),
+            st.Page('pages/page2_sensors.py', title="Sensors"),
+            st.Page('pages/page3_production.py', title="Production"),
+            st.Page('pages/page3b_production_movement.py', title="Ownership and Production Trends"),
+            st.Page('pages/page4_environmental.py', title="Environmental Impacts")
+            
+        ],
+        "External Datasets": [
+            st.Page('pages/page5_other.py', title="Other Visualizations")
+        ],
+    }
 
 pg = st.navigation(pages)
-pg.run()
 
+### User Authentication
+names = ["aquaculture chile app"]
+usernames = ["aquaculture-chile-app"]
 
+file_path = Path(__file__).parent / "config/hashed_pw.pkl"
+with file_path.open("rb") as file:
+    hashed_passwords = pickle.load(file)
+    
+authenticator = stauth.Authenticate(
+    names=names,
+    usernames=usernames,
+    passwords=hashed_passwords,
+    cookie_name="aqua_cookie",
+    key="abcdef",
+    cookie_expiry_days=5,
+)
 
+name, authentication_status, username = authenticator.login("Aquaculture Chile Login", "main")
 
-# page = st.sidebar.selectbox("Select the Page to Explore", ["Home", "Concessions", "Sensors Exploration", "Production Exploration", "Complaints Exploration"])
-
-# if page == "Home":
-#     splash_page()
-# elif page == "Concessions Exploration":
-#     concessions_exploration_page()
-# elif page == "Sensors Exploration":
-#     sensors_exploration_page()
-# elif page == "Production Exploration":
-#     production_exploration_page()
-# elif page == "Complaints Exploration":
-# page = st.sidebar.selectbox("Select the Page to Explore", ["Home", "Concessions Exploration", "Sensors Exploration", "Production Exploration", "Complaints Exploration"])
-
-# if page == "Home":
-#     splash_page()
-# elif page == "Concessions Exploration":
-#     concessions_exploration_page()
-# elif page == "Sensors Exploration":
-#     st.write("This is the sensors exploration page")
-# elif page == "Production Exploration":
-#     st.write("This is the production exploration page")
-# elif page == "Complaints Exploration":
-#     st.write("This is the complaints exploration page")
+if authentication_status == False:
+    st.error("Username/password is incorrect")
+elif authentication_status == None:
+    st.warning("Please enter your username and password")
+elif authentication_status == True:
+    
+    
+    def finish_session():
+        authenticator.logout("End Session", "sidebar")
+    
+    st.sidebar.button("End Session", on_click=finish_session)
+    pg.run()
+    st.sidebar.write(f"Welcome, {name}!")
--- aqua-app/pages/page0_home.py
+++ aqua-app/pages/page0_home.py
@@ -3,17 +3,21 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import time
+from pyprojroot import here
 
+# set root directory
+root = here()
 
-# center title
-st.title("Welcome to the Chile Aquaculture Project")
+# spinner until the entire page is loaded
+with st.spinner("Loading page..."):
+    time.sleep(3)
 
+# @st.cache_data(show_spinner="Loading map...")
 # insert html map from /Users/victholl/Documents/aquaculture_chile/vict_playground/concession_eda.html
-@st.cache_data(show_spinner="Loading map...")
 def load_html_map():
-    with open('media/concession_eda_production_bubble_map2.html', 'r') as file:
+    with open(root / 'media' / 'concession_eda_production_bubble_map2.html', 'r') as file:
         html_map = file.read()
     return html_map
-
-# make map bigger height and width
+st.title("Welcome to the Chile Aquaculture Project")
 st.components.v1.html(load_html_map(), width="100%", height=900)
